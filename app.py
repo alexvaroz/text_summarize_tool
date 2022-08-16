9b7bf8894d2976755fff1efc409e423dcb7a9ada
alexandrevr <alexandre.vaz@gmail.com>
2022-08-16 13:04:18 -0300

    Primeira versão

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 from utils import cleanup_sentences, summarize, nlkt_word_tokenize, read_article
 from gensim.models import Word2Vec
 
-st.title("Minha primeira aplicação")
+st.title("Sumarização de notícias")
 
-st.markdown("#### Minha primera aplicação realizará um resumo de um texto inserido.")
-url = st.text_input("")
+st.write(" Esta aplicação apresentará as sentenças mais importantes do texto presente na URL inserida.")
+valor_url = ""
+url = st.text_input("Insira a url", valor_url)
 
 words = []
 if len(url) > 1:
@@ -15,7 +16,11 @@ if len(url) > 1:
         words.append(nlkt_word_tokenize(sent))
     model = Word2Vec(words, min_count=1, sg=1)
     result = summarize(text, model)
+    st.subheader("Resumo:")
     st.write(result)
 
 st.sidebar.header("About")
+if st.sidebar.checkbox('Mostrar texto completo:'):
+    st.subheader("Texto completo:")
+    st.write(text)
 st.sidebar.subheader("Essa aplicação foi desenvolvida por Alexandre Vaz")
